77b00b1e0257e9ee5cf7f6a86e4ab30ed0d39b2a
Luke <luke.stephens@trintel.co.za>
2023-09-22 16:34:05 +0200

    make unique component

--- components/transActionCreation/transActionCreation.tsx
+++ components/transActionCreation/transActionCreation.tsx
@@ -2,27 +2,17 @@ import React, { FunctionComponent, useState } from "react";
 import { Text, View } from "react-native";
 import { PickYourPaymates } from "./pickYourPaymates";
 import { Toggle } from "../toggle/toggle";
+import { WhoOwesWho } from "./whoOwesWho";
 
 interface OwnProps {}
 
 type Props = OwnProps;
 
 export const TransActionCreation: FunctionComponent<Props> = (props) => {
-  const [isActive, setIsActive] = useState(false);
-
   return (
     <View className={`flex flex-col p-4 pr-0`}>
       <PickYourPaymates />
-      <View
-        className={`
-          flex flex-row items-center mt-8 w-full justify-between pr-4
-      `}
-      >
-        <Text className={`text-zinc-400 text-md font-medium`}>
-          {isActive ? "I owe them" : "They owe me"}
-        </Text>
-        <Toggle isActive={isActive} setIsActive={setIsActive} />
-      </View>
+      <WhoOwesWho />
     </View>
   );
 };
